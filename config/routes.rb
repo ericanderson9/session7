a21b653597c263bc2ea0018190801183dea6bcd8
Eric Anderson <eric.anderson1@kellogg.northwestern.edu>
2024-05-10 16:31:24 +0000

    end of class

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   get("/tacos", :controller => "tacos", :action => "index")
   # resources "dice"
   get("/dice", :controller => "dice", :action => "index")
-  # resources "cards"
-  get("/cards", :controller => "cards", :action => "index")
-  # resources "bitcoins"
-  get("bitcoins", :controller => "bitcoins", :action => "index")
-  # resources "companies"
-  get("/companies", :controller => "companies", :action => "index")
-  # resources "contacts"
+  resources "cards"
+  # get("/cards", :controller => "cards", :action => "index")
+  resources "bitcoins"
+  # get("/bitcoins", :controller => "bitcoins", :action => "index")
+  resources "companies"
+  # get("/companies", :controller => "companies", :action => "index")
+  # get("/companies/:id", :controller => "companies", :action => "show")
+  resources "contacts"
   get("/contacts", :controller => "contacts", :action => "index")
-  # resources "posts"
-  get("/posts", :controller => "posts", :action => "index")
+  resources "posts"
+  # get("/posts", :controller => "posts", :action => "index")
 end
